8e141158a85f5404f0fa99050d6aa5553c3f9942
charlycator <devcharly@pm.me>
2024-03-16 19:20:35 +0100

    feat: bfs comment explanation

--- lib/bfs-tree-search.ts
+++ lib/bfs-tree-search.ts
@@ -1,3 +1,13 @@
+/*
+ BFS SEARCH:
+
+ BFS is used to traverse or search in data structures like a graph or a tree, but here we are considering a
+ tree for simplicity. The search starts from the root (topmost) node, then it visits all the nodes at the
+ present depth or 'distance' from the root before moving to nodes of the next depth level.
+
+ It moves broadly through our tree, checking all the 'siblings' (nodes with the same parent) before it moves down
+ to the next level of children.
+*/
 export interface Node {
   name: string;
   children: Node[];
