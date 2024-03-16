89930d32d666626c66ae1fb013037e38795eda13
charlycator <devcharly@pm.me>
2024-03-16 19:24:57 +0100

    feat: binary search comment explanation

--- lib/binary-search.ts
+++ lib/binary-search.ts
@@ -1,3 +1,14 @@
+/*
+ BINARY SEARCH:
+
+ It is a search algorithm that finds the position of a target value within a sorted array or list.
+ It compares the target value to the middle element of the array; if they are unequal, the half in
+ which the target cannot lie is eliminated, and the search continues on the remaining half until it
+ is successful or the remaining half is empty.
+
+ For example, when you look for a word in a dictionary, In each step, you are effectively halving
+ the part of the dictionary you need to look through.
+*/
 const binarySearch = (arr: number[], target: number): number => {
   let left = 0;
   let right = arr.length - 1;
