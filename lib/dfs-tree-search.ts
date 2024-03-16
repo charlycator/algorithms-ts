163077d5fc7e8af384c091cbb40516f6384a5e9f
charlycator <devcharly@pm.me>
2024-03-16 19:22:38 +0100

    feat: dfs comment explanation

--- lib/dfs-tree-search.ts
+++ lib/dfs-tree-search.ts
@@ -1,3 +1,13 @@
+/*
+ DFS SEARCH:
+
+ The algorithm starts at the root of a binary tree and goes as far as possible along each branch before backtracking.
+ This means you explore one path fully before backing up and trying an unexplored path.
+ In a binary tree, DFS would explore all the way down one side of the tree (say left or low in a binary search)
+ until it can't go any further, then it will backtrack and explore the other side. (Say right or high
+ in a binary search.)
+
+*/
 export function dfs(graph: number[][], startNode: number, visited: boolean[] = []): void {
   console.log(startNode);
 
